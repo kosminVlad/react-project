f00d074f7e8c92d183f7568746ae75bfa32cbfb1
kosmin <ksagwey.vlad@mail.ru>
2024-02-14 18:38:01 +0700

    123

--- app/page.js
+++ app/page.js
@@ -1,17 +1,20 @@
+import { getGamesByCategory } from "./data/data-utils"; 
 import { Banner } from "@/app/components/Banner/Banner"
 import { Promo } from "./components/Promo/Promo"
-import { PopularCardsList } from "./components/CardsList/PopularCardsList/PopularCardsList"
-import { NewCardsList } from "./components/CardsList/NewCardsList/NewCardsList"
+import { CardsList } from "./components/CardsList/CardsList"
 import Image from "next/image";
 import styles from "./page.module.css";
 
 
 export default function Home() {
+  const popularGames = getGamesByCategory("popular");
+  const newGames = getGamesByCategory("new");
+
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id="popular" title="Популярные" data={popularGames}/>
+      <CardsList id="new" title="Новинки" data={newGames}/>
       <Promo />
     </main>
   )

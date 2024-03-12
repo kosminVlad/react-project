b928c92ef651fd3c6bb03bc126a5176d65ae3830
kosmin <ksagwey.vlad@mail.ru>
2024-03-12 16:11:50 +0700

    123

--- app/page.js
+++ app/page.js
@@ -1,31 +1,15 @@
-"use client";
-import { getGamesByCategory } from "./data/data-utils";
+import { getNormalizedGamesDataByCategory } from "./api/api-utils";
 import { Banner } from "./components/Banner/Banner";
 import { CardsList } from "./components/CardsList/CardsList";
 import { Promo } from "./components/Promo/Promo";
-import { useEffect } from "react";
+import { endpoints } from "./api/config";
+import { getData } from "./api/api-utils";
 
-export default function Home() {
-  useEffect(() => {
-    const getData = async (url) => {
-      try {
-        const response = await fetch(url);
-        if (response.status !== 200) {
-          throw new Error("Ошибка получения данных");
-        }
-        const data = await response.json();
-        console.log(data);
-        return data;
-      } catch (error) {
-        return error;
-      }
-    };
-    getData("https://api-code-2.practicum-team.ru/games");
-  }, []);
+export default async function Home() {
 
-  const popularGames = getGamesByCategory("popular");
-  const newGames = getGamesByCategory("new");
-
+  const popularGames = await getNormalizedGamesDataByCategory(endpoints.games, "popular")
+  const newGames = await getNormalizedGamesDataByCategory(endpoints.games, "new")
+  
   return (
     <main className="main">
       <Banner />
